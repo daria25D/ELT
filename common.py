8a44210cb43c3267810a8eda6772ce03ee49d5e3
daria25D <dasha_domracheva@mail.ru>
2022-04-08 18:22:43 +0300

    Add common data, rewrite stopping thread

---
+++ common.py
@@ -0,0 +1,12 @@
+# Constants
+
+BATCH = 3
+
+MAX_TEMP = 100
+MIN_TEMP = 0
+
+TEMP_PERIOD = 1
+GEN_LOG_FILE = 'log_gen.txt'
+
+HOST = 'localhost'
+REDIS_QUEUE = 'redis_common'
--- manager.py
+++ manager.py
@@ -1,3 +1,4 @@
+from common import *
 from datetime import datetime
 import random
 import redis
@@ -6,32 +7,10 @@ import threading
 import time
 
 
-MAX_TEMP = 100
-MIN_TEMP = 0
-
-TEMP_PERIOD = 1
-GEN_LOG_FILE = 'log_gen.txt'
-
-HOST = 'localhost'
-REDIS_QUEUE = 'redis_common'
-
-
-def wait_thread_end(t):
-    try:
-        t.join()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            sys.exit(1)
-    finally:
-        t.stop()
-
-
 class GeneratorThread(threading.Thread):
     def __init__(self, log_file_name: str):
         super().__init__()
+        self._stop = threading.Event()
         self.log_file_name = log_file_name
         # establish connection to Redis
         self.r = redis.Redis(host=HOST, port=6379, db=0)
@@ -42,20 +21,20 @@ class GeneratorThread(threading.Thread):
     def run(self):
         self.running = 1
         self.log_file = open(self.log_file_name, "w", 1) # flush data
-        while self.running:
+        while not self.stopped():
             temperatures = [random.randint(MIN_TEMP, MAX_TEMP)]
             first_time = datetime.now().strftime("%H:%M:%S")
             if not self.log_file.closed:
                 self.log_file.write(f'{first_time}: {temperatures[0]}\n')
             time.sleep(TEMP_PERIOD)
-            for i in range(2):
+            for i in range(1, BATCH):
                 temperature = random.randint(MIN_TEMP, MAX_TEMP)
                 cur_time = datetime.now().strftime("%H:%M:%S")
                 temperatures.append(temperature)
                 if not self.log_file.closed:
                     self.log_file.write(f'{cur_time}: {temperature}\n')
                 time.sleep(TEMP_PERIOD)
-            print(first_time, temperatures)
+            # print(first_time, temperatures)
             self.send_data_to_redis(first_time, temperatures)
 
     def send_data_to_redis(self, key_time, temperatures):
@@ -64,21 +43,34 @@ class GeneratorThread(threading.Thread):
         except redis.ConnectionError:
             print("Redis connection lost")
 
-    def stop_running(self):
-        self.running = 0
-        print("Terminating generation")
-
     def stop_logging(self):
         self.log_file.close()
-        print("Terminating logging")
 
     def stop_redis_connection(self):
         self.r.close()
 
+    def stopped(self):
+        return self._stop.isSet()
+
     def stop(self):
-        self.stop_running()
+        print("Terminating generation")
+        self._stop.set()
+        print("Terminating logging")
         self.stop_logging()
         self.stop_redis_connection()
+
+
+def wait_thread_end(t: GeneratorThread):
+    try:
+        t.join()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            sys.exit(1)
+    finally:
+        t.stop()
 
 
 def main():
