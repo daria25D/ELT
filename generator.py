d9c4cf20fac135db7474938f4a441ad56dadd953
daria25D <dasha_domracheva@mail.ru>
2022-04-08 18:41:17 +0300

    Rearrange generation

---
+++ generator.py
@@ -0,0 +1,63 @@
+from common import *
+from datetime import datetime
+import random
+import redis
+import threading
+import time
+
+
+class GeneratorThread(threading.Thread):
+    def __init__(self, log_file_name: str):
+        super().__init__()
+        self._stop = threading.Event()
+        self.log_file_name = log_file_name
+        # establish connection to Redis
+        self.r = redis.Redis(host=HOST, port=6379, db=0)
+        self.redis_id = REDIS_QUEUE
+        if self.r.exists(self.redis_id):  # some leftovers
+            self.r.delete(self.redis_id)
+
+    def generate_temperatures(self, batch_num=0):
+        temperatures = []
+        times = []
+        for i in range(BATCH):
+            temperature = random.randint(MIN_TEMP, MAX_TEMP)
+            cur_time = datetime.now().strftime("%H:%M:%S")
+            temperatures.append(temperature)
+            times.append(cur_time)
+            if not self.log_file.closed:
+                self.log_file.write(f'{batch_num} {cur_time}: {temperature}\n')
+            time.sleep(TEMP_PERIOD)
+        # print(first_time, temperatures)
+        self.send_data_to_redis(times[0], temperatures)
+
+    def send_data_to_redis(self, key_time, temperatures):
+        try:
+            if not self.stopped(): # do not record last batch of data generated on interrupt ?
+                print(key_time, *temperatures)
+                self.r.lpush(key_time, *temperatures)
+        except redis.ConnectionError:
+            print("Redis connection lost")
+
+    def run(self):
+        self.log_file = open(self.log_file_name, "w", 1) # flush data
+        batch_num = 1
+        while not self.stopped():
+            self.generate_temperatures(batch_num)
+            batch_num += 1
+
+    def stop_logging(self):
+        self.log_file.close()
+
+    def stop_redis_connection(self):
+        self.r.close()
+
+    def stopped(self):
+        return self._stop.is_set()
+
+    def stop(self):
+        print("Terminating generation")
+        self._stop.set()
+        print("Terminating logging")
+        self.stop_logging()
+        self.stop_redis_connection()
--- manager.py
+++ manager.py
@@ -1,63 +1,11 @@
-from common import *
+from common import GEN_LOG_FILE
+from generator import GeneratorThread
 from datetime import datetime
 import random
 import redis
 import sys
 import threading
 import time
-
-
-class GeneratorThread(threading.Thread):
-    def __init__(self, log_file_name: str):
-        super().__init__()
-        self._stop = threading.Event()
-        self.log_file_name = log_file_name
-        # establish connection to Redis
-        self.r = redis.Redis(host=HOST, port=6379, db=0)
-        self.redis_id = REDIS_QUEUE
-        if self.r.exists(self.redis_id):  # some leftovers
-            self.r.delete(self.redis_id)
-
-    def run(self):
-        self.running = 1
-        self.log_file = open(self.log_file_name, "w", 1) # flush data
-        while not self.stopped():
-            temperatures = [random.randint(MIN_TEMP, MAX_TEMP)]
-            first_time = datetime.now().strftime("%H:%M:%S")
-            if not self.log_file.closed:
-                self.log_file.write(f'{first_time}: {temperatures[0]}\n')
-            time.sleep(TEMP_PERIOD)
-            for i in range(1, BATCH):
-                temperature = random.randint(MIN_TEMP, MAX_TEMP)
-                cur_time = datetime.now().strftime("%H:%M:%S")
-                temperatures.append(temperature)
-                if not self.log_file.closed:
-                    self.log_file.write(f'{cur_time}: {temperature}\n')
-                time.sleep(TEMP_PERIOD)
-            # print(first_time, temperatures)
-            self.send_data_to_redis(first_time, temperatures)
-
-    def send_data_to_redis(self, key_time, temperatures):
-        try:
-            self.r.lpush(key_time, *temperatures)
-        except redis.ConnectionError:
-            print("Redis connection lost")
-
-    def stop_logging(self):
-        self.log_file.close()
-
-    def stop_redis_connection(self):
-        self.r.close()
-
-    def stopped(self):
-        return self._stop.isSet()
-
-    def stop(self):
-        print("Terminating generation")
-        self._stop.set()
-        print("Terminating logging")
-        self.stop_logging()
-        self.stop_redis_connection()
 
 
 def wait_thread_end(t: GeneratorThread):
